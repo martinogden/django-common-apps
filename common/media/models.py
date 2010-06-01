a49f5a0d39fa9c6c24f060e31c049e47de642ab6
Martin Ogden <martin@cahoona.co.uk>
2010-06-01 14:27:29 +0000

    Cleaned up media models
    git-svn-id: http://code.cahoona.co.uk/django_reuseable/trunk@14 8f0c2b07-f46b-4a3b-9cf6-4e2ffc571f9c

--- common/media/models.py
+++ common/media/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
+from django.core.exceptions import ValidationError
+from django.conf import settings
 
 class Image(models.Model):
   local_image = models.ImageField(upload_to="uploads/%Y/%m/%d/", height_field="height", width_field="width", max_length=255, null=True, blank=True)
@@ -42,31 +44,63 @@ class Video(models.Model):
     ('youtube', 'youtube.com'),
     ('vimeo', 'vimeo.com'),
   )
-  url = models.URLField(max_length=255)
-  provider = models.TextField(max_length=10, choices=PROVIDER_CHOICES)
+  url = models.URLField(max_length=255, help_text="Copy the url from te address bar for the Vimeo or youtube video. Do NOT copy the ambed code.")
+  provider = models.TextField(max_length=10, choices=PROVIDER_CHOICES, editable=False)
   content_type = models.ForeignKey(ContentType)
   object_id = models.PositiveIntegerField()
   content_object = generic.GenericForeignKey('content_type', 'object_id')
   created_at = models.DateTimeField(editable=False, auto_now_add=True)
   updated_at = models.DateTimeField(editable=False, auto_now=True)
   
+  def clean(self):
+    # Only allow for Youtube or Vimeo
+    if self.url.find("youtube.com") is -1 and self.url.find("vimeo.com") is -1:
+      raise ValidationError(u'Video must be from either Vimeo or Youtube')
+      
+  def save(self):
+    # Decide which provider video is from:
+    if self.url.find("youtube.com"):
+      self.provider = "youtube.com"
+    elif self.url.find("vimeo.com"):
+      self.provider = "vimeo.com"
+      
+    super(Video, self).save()
+    
   def __unicode__(self):
-    return u'%s' % (self.url) # TODO: REGEX videos and resize effectively, and abstact sizes to settings.py
-  
+    if self.provider == "vimeo.com":
+      video_id = self.url.split("/")[-1]
+      return u'<object width="%s" height="%s"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=%s&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=%s&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="%s" height="%s"></embed></object>' % (settings.VIDEO_WIDTH, settings.VIDEO_HEIGHT, video_id, video_id, settings.VIDEO_WIDTH, settings.VIDEO_HEIGHT)
+    elif self.provider == "youtube.com":
+      video_id = self.url.split("v=")[-1]
+      return u'<object width="%s" height="%s"><param name="movie" value="http://www.youtube.com/v/%s&hl=en_US&fs=1&"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/%s&hl=en_US&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="%s" height="%s"></embed></object>' % (settings.VIDEO_WIDTH, settings.VIDEO_HEIGHT, video_id, video_id, settings.VIDEO_WIDTH, settings.VIDEO_HEIGHT)
+      
 class Audio(models.Model):
   PROVIDER_CHOICES = (
     ('soundcloud', 'soundcloud.com'),
   )
-  embed = models.TextField(help_text="Add &lt;embed&gt;&lt;/embed&gt; code here.")
+  link = models.URLField(help_text="Add link here, NOT embed code.", max_length=255)
   provider = models.TextField(editable=False, max_length=10, choices=PROVIDER_CHOICES)
   content_type = models.ForeignKey(ContentType)
   object_id = models.PositiveIntegerField()
   content_object = generic.GenericForeignKey('content_type', 'object_id')
   created_at = models.DateTimeField(editable=False, auto_now_add=True)
   updated_at = models.DateTimeField(editable=False, auto_now=True)
-
+  
+  class Meta:
+    verbose_name_plural = "Audio"
+  def clean(self):
+    # Only allow for Youtube or Vimeo
+    if self.url.find("soundcloud.com") is -1:
+      raise ValidationError(u'Audio must be from Soundcloud')
+      
+  def save(self):
+    if self.url.find("soundcloud.com"):
+      self.provider = "soundcloud.com"
+    super(Audio. self).save()
+    
   def __unicode__(self):
-    return u'%s' % (self.embed)
+    if self.provider == "soundcloud.com":
+      return u'<object width="%s" height="%s"><param name="movie" value="http://player.soundcloud.com/player.swf?url=%s&&color=0066cc"></param><param name="allowscriptaccess" value="always"></param><embed allowscriptaccess="always" src="http://player.soundcloud.com/player.swf?url=%s&&color=0066cc" type="application/x-shockwave-flash" width="%s" height="%s"></embed></object>' % (settings.SOUNDCLOUD_WIDTH, settings.SOUNDCLOUD_HEIGHT, self.url, self.url, settings.SOUNDCLOUD_WIDTH, settings.SOUNDCLOUD_HEIGHT)
     
 class File(models.Model):
   path = models.FileField(upload_to="uploads/%Y/%m/%d/", max_length=255)
