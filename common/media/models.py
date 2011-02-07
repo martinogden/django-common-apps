6883b3c5747cbe39ffe9804cde6bf2de52cd845f
Martin Ogden <martin@cahoona.co.uk>
2011-02-07 19:50:35 +0000

    Checked in from server
    git-svn-id: http://code.cahoona.co.uk/django_reuseable/trunk@26 8f0c2b07-f46b-4a3b-9cf6-4e2ffc571f9c

--- common/media/models.py
+++ common/media/models.py
@@ -78,15 +78,28 @@ class Video(models.Model):
     elif self.provider == "youtube.com":
       video_id = self.url.split("v=")[-1]
       return SafeUnicode('<object width="%s" height="%s"><param name="movie" value="http://www.youtube.com/v/%s&hl=en_US&fs=1&"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/%s&hl=en_US&fs=1&" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="%s" height="%s"></embed></object>' % (width, height, video_id, video_id, width, height))
-     
-  @property 
-  def video_id(self):
-    if self.provider == "vimeo.com":
-      return self.url.split("/")[-1]
-    elif self.provider == "youtube.com":
-      return self.url.split("v=")[-1]
 
-
+  # returns a thumbnail for the video  
+  def get_thumbnail(self):
+      if self.provider == "vimeo.com":
+          try:
+              import urllib2
+              from xml.dom import minidom
+              # get thumbnail using api (need to set user agent as vimeo's api doesn't like urllib for some reason)
+              video_id = self.url.split("/")[-1]
+              handle = urllib2.build_opener()
+              request = urllib2.Request("http://vimeo.com/api/v2/video/%s.xml" % (video_id))
+              request.add_header("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.7) Gecko/20091221 Firefox/3.5.7 GTB6 (.NET CLR 3.5.30729)")
+              data = minidom.parseString(handle.open(request).read())
+              thumbnail = data.getElementsByTagName("thumbnail_large")[0].firstChild.nodeValue
+              return thumbnail
+          except:
+              return ""
+      elif self.provider == "youtube.com":
+          # get thumbnail via http
+          video_id = self.url.split("v=")[-1]
+          return "http://img.youtube.com/vi/%s/0.jpg" % video_id
+      
 class Audio(models.Model):
   PROVIDER_CHOICES = (
     ('soundcloud', 'soundcloud.com'),
@@ -116,6 +129,15 @@ class Audio(models.Model):
       # Seems links are quite hard to extract from soundcloud share button etc.
       # return SafeUnicode('<object width="%s" height="%s"><param name="movie" value="http://player.soundcloud.com/player.swf?url=%s&&color=0066cc"></param><param name="allowscriptaccess" value="always"></param><embed allowscriptaccess="always" src="http://player.soundcloud.com/player.swf?url=%s&&color=0066cc" type="application/x-shockwave-flash" width="%s" height="%s"></embed></object>' % (settings.SOUNDCLOUD_WIDTH, settings.SOUNDCLOUD_HEIGHT, self.url, self.url, settings.SOUNDCLOUD_WIDTH, settings.SOUNDCLOUD_HEIGHT))
       return SafeUnicode(self.embed)
+
+     
+  @property 
+  def video_id(self):
+    if self.provider == "vimeo.com":
+      return self.url.split("/")[-1]
+    elif self.provider == "youtube.com":
+      return self.url.split("v=")[-1]
+
       
 class File(models.Model):
   path = models.FileField(upload_to="uploads/%Y/%m/%d/", max_length=255)
