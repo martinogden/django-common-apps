906a79f2ce2989e02755635e4c07a1f1e3a6a34e
Martin Ogden <martin.ogden@gmail.com>
2011-02-26 12:44:15 +0000

    Add setup file from nathan borrors basic apps

--- setup.py
+++ setup.py
@@ -1,8 +1,39 @@
-from setuptools import setup
+# Borrowed from https://github.com/nathanborror/django-basic-apps setup.py
+import os
+from distutils.core import setup
 
 
-setup(
-    name="django-common-apps",
-    packages=['common',],
-)
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == "":
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
 
+
+package_dir = "basic"
+
+
+packages = []
+for dirpath, dirnames, filenames in os.walk(package_dir):
+    # ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith("."):
+            del dirnames[i]
+    if "__init__.py" in filenames:
+        packages.append(".".join(fullsplit(dirpath)))
+
+
+setup(name='django-common-apps',
+    version='0.1',
+    description='Django Common Apps',
+    author='Martin Ogden',
+    url='http://github.com/martinogden/django-common-apps',
+    packages=packages)
